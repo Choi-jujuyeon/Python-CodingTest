356a48e19d0ebb893f8ed98e8f17ae6a426ce034
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-02-19 21:23:03 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -1,7 +1,2 @@
 def solution(date1, date2):
-    a=''
-    b=''
-    for i in range(3):
-        a+=str(date1[i])
-        b+=str(date2[i])
-    return 1 if int(a)<int(b) else 0
+    return int(date1<date2)
