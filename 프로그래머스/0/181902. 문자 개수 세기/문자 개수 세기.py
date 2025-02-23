b1af5fe5b029a8ed1375c1bc52e1d2abfab387d3
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-02-23 22:30:21 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,10 +1,8 @@
-from collections import Counter
 def solution(my_string):
-    c=Counter(my_string)
-    a=[]
-    for i in range(ord('A'),ord('Z')+1):
-        a.append(c.get(chr(i),0))
-    for i in range(ord('a'),ord('z')+1):
-        a.append(c.get(chr(i),0))
+    a=[0]*52
+    for i in my_string:
+        if i.isupper():
+            a[ord(i)-65]+=1
+        else:
+            a[ord(i)-71]+=1
     return a
-        
