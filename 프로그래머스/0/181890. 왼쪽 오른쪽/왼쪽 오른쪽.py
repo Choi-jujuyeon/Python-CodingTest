48d3c4327b2b52365d1f2bdec65e722fd76ed22e
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-02-23 23:31:51 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,9 +1,9 @@
-def solution(str_list):
-    if 'l' in str_list or 'r'in str_list: 
-        for i in str_list:
-            if i=='l':
-                return str_list[:str_list.index('l')]
-            elif i=='r':
-                return str_list[str_list.index('r')+1:]
-    else:
-        return []
+def solution(A):
+    for i in A:
+        if i=='l':
+            return A[:A.index(i)]
+        elif i=='r':
+            return A[A.index(i)+1:]
+    return []
+    
+  
