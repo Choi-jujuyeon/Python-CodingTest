a436ed4ca758360e9d5fd7bb03bf2001747fbbf2
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-02-27 21:25:36 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -1,9 +1,2 @@
-from collections import Counter
 def solution(array, n):
-    for k,v in Counter(array).items():
-        if k==n:
-            return v
-        
-    return 0
-    
-        
+    return array.count(n)
