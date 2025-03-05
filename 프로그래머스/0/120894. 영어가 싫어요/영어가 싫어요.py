16d274164416c3a4b66136dea33544aaff2dd62a
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-03-05 18:10:41 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,8 +1,5 @@
 def solution(numbers):
-    for v,k in [
-        ("1", "one"), ("2", "two"), ("3", "three"),
-        ("4", "four"), ("5", "five"), ("6", "six"),
-        ("7", "seven"), ("8", "eight"), ("9", "nine"),("0","zero")
-    ]:
-        numbers=numbers.replace(k,str(v))
+    for i,num in enumerate(["zero","one","two","three","four","five","six","seven","eight","nine"]):
+        numbers=numbers.replace(num,str(i))
     return int(numbers)
+    
