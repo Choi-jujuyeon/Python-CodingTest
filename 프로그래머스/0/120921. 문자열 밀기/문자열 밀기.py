1834b335811da5fbe82c0d0f8b9bc37096e5a78a
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-03-06 21:28:11 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,14 +1,2 @@
 def solution(A, B):
-    count=-1
-    for i in range(1,len(A)+1):
-        if A==B:
-            count=0
-            break
-        elif A[-i:]+A[:-i]==B:
-            count=i
-            break
-    return count
-    
-            
-        
-        
+    return (B*2).find(A)
