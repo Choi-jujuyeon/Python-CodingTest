face2694923c5f6abce3c514adba0da027f96aae
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-03-12 00:33:17 +0900

    [level 1] Title: 소수 찾기, Time: 633.50 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,8 +1,7 @@
 def solution(n):
-    arr=set([i for i in range(1,n+1)])
+    arr=set(range(2,n+1))
     
-    for i in range(2,int(max(arr)**0.5)+1):
-        arr-=set(range(i*2,max(arr)+1,i))
-    arr-=set(range(0,2))
+    for i in range(2,int(n**0.5)+1):
+        arr-=set(range(i*2,n+1,i))
     
     return len(arr)
