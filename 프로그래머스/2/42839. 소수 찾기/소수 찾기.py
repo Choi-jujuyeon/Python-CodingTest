02e8e06d13eb100db19dc2bb5d5d5af5084c869c
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-03-12 00:09:29 +0900

    [level 2] Title: 소수 찾기, Time: 924.05 ms, Memory: 281 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,18 +1,22 @@
 from itertools import permutations
+
 def solution(numbers):
+    result_set=set()
     
-    #step01. 모든 숫자의 조합을 만든다.
-    prime_set=set()
+    #01. 모든 조합 만들기
     for i in range(len(numbers)):
-        numbers_permutation=permutations(list(numbers), i+1)       
-        numbers_per_list=list(map(int,map("".join,numbers_permutation)))        
-        prime_set |=set(numbers_per_list)
+        #print(list(permutations(numbers,i+1)))      #permutations():문자열,리스트->iterable 가능
+        com=list(map(int,map("".join,list(permutations(numbers,i+1)))))
+        result_set|=set(com)
+        
+    #02. 에라토스테네스의 체 사용
+    result_set-=set(range(0,2))
+    end=int(max(result_set)**0.5)+1
     
-    #step 02.0과 1을 집합으로 만들고 빼쥐
-    prime_set-=set(range(0,2))
+    for i in range(2,end):      #배수 값 빼주기
+        result_set-=set(range(i*2,max(result_set)+1,i))
     
-    #step03. 에라토스테네스의 체 활용
-    lim=int(max(prime_set)**0.5)+1       #핵심코드
-    for i in range(2,lim):
-        prime_set -= set(range(i*2,max(prime_set)+1,i))
-    return len(prime_set)  
+    return len(result_set)
+    
+    
+        
