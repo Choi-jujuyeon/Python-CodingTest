6e6e984f0ccb332a88d0d3fc82decd8ec2c386d7
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-03-16 23:57:02 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,15 +1,10 @@
+from collections import Counter
+from functools import reduce
 def solution(clothes):
-    #1.옷을 종류 별로 구분한다
-    hash_map={}
-    for cloth,type in clothes:
-        hash_map[type]=hash_map.get(type,0)+1
-    print(hash_map)
-     
-    #2.입지 않는 경우를 추가하여 모든 조합을 계산한다.
-    result=1
-    for type in hash_map:
-        result*=(hash_map[type]+1)
         
-    #3.아무 종류의 옷도 입지 않는 경우를 제외한다
+    counter=Counter([type for cloth,type in clothes])
+    
+    result=reduce(lambda k,v:k*(v+1),counter.values(),1)
     return result-1
     
+        
