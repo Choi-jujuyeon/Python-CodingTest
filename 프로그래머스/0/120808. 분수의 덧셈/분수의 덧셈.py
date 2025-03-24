8dd8603e92754feb1c6cf507e855468db219cb77
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-03-24 18:34:10 +0900

    [level 0] Title: 분수의 덧셈, Time: 65.72 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,6 +1,14 @@
-import math
-def solution(numer1, denom1, numer2, denom2):
-    dd=denom1*denom2
-    uu=numer1*denom2 + numer2*denom1
-    return [uu//math.gcd(dd,uu), dd//math.gcd(dd,uu)]
-    
+def solution(denum1, num1, denum2, num2):
+    boonja = denum1 * num2 + denum2 * num1
+    boonmo = num1 * num2
+
+    start = max(boonja, boonmo)
+    gcd_value = 1
+
+    for i in range(start, 0, -1):
+        if boonmo % i == 0 and boonja % i == 0:
+            gcd_value = i
+            break
+
+    answer = [boonja // gcd_value, boonmo // gcd_value]
+    return answer
