baf79a7d0e4bc5b507b49e9e974404940ceaa138
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-03-25 19:09:13 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.04 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,26 @@
+def solution(dartResult):
+    result=[]
+    prev=0
+    dartResult=dartResult.replace(str(10),'a')
+    for i in dartResult:
+        if i=='a':
+            prev=10
+        if i.isdigit():
+            prev=int(i)
+        elif i=='S':
+            result.append(prev)
+        elif i=='D':
+            result.append(prev**2)
+        elif i=='T':
+            result.append(prev**3)
+        elif i=='*':
+            if len(result)==1:
+                result[-1]=result[-1]*2
+            else:
+                result[-1]*=2
+                result[-2]*=2
+        elif i=='#':
+            result[-1]*=-1
+    return sum(result)
+        
+    
