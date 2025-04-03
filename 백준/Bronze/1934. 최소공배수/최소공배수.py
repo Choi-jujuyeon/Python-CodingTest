0a15dafb51c75e37b5ccb784369a0282adff6d82
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-04-03 16:17:33 +0900

    [Bronze I] Title: 최소공배수, Time: 88 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -2,8 +2,4 @@ import math
 a=int(input())
 for i in range(a):
     c,d=map(int,input().split())
-    gcd_val=math.gcd(c,d)
-    if c==1 or d==1:
-        print(c*d)
-    else:
-        print(c//gcd_val * d//gcd_val*gcd_val)
+    print(math.lcm(c,d))
