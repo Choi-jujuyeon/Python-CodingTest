947777ef447d9bedd8df429ef6ea352ef3c90cd5
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-05-12 22:16:04 +0900

    [D1] Title: [파이썬 프로그래밍 기초(1) 파이썬의 기본 구조와 기초 문법] 9. 내장함수 4, Time: 56 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D1/6311. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 9． 내장함수 4/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 9． 내장함수 4.py
+++ SWEA/D1/6311. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 9． 내장함수 4/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 9． 내장함수 4.py
@@ -1,14 +1,4 @@
-from collections import Counter
 a="ADCBBBBCABBCBDACBDCAACDDDCAABABDBCBCBDBDBDDABBAAAAAAADADBDBCBDABADCADC"
-result=0
-
-for k,v in Counter(a).items():
-    if k=="A":
-        result+=v*4
-    elif k=='B':
-        result+=v*3
-    elif k=='C':
-        result+=v*2
-    else:
-        result+=v*1
-print(result)
+s={'A':4, 'B': 3, 'C': 2, 'D':1}
+s_arr=list(map(lambda x: s[x], a))
+print(sum(s_arr))
