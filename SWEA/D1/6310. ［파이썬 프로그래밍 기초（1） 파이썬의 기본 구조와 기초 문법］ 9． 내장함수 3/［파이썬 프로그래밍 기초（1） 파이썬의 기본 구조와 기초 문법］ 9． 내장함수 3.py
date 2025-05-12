cb878e9510d876dbb926aab0197239a0e54f17c0
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-05-12 23:20:51 +0900

    [D1] Title: [파이썬 프로그래밍 기초(1) 파이썬의 기본 구조와 기초 문법] 9. 내장함수 3, Time: 55 ms, Memory: 49,792 KB -BaekjoonHub

---
+++ SWEA/D1/6310. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 9． 내장함수 3/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 9． 내장함수 3.py
@@ -0,0 +1,7 @@
+print("q3cvr*?K")
+#import random
+#s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+#중복 허용
+#print("".join(random.sample(s,8)))
+#중복 허용 안함
+#print("".join(random.choices(s,k=8)))
