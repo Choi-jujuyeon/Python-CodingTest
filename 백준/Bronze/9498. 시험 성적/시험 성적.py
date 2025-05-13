5eeefa42fddbb52357f634116ad30737cd9c0ce5
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-05-14 00:00:22 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,11 +1,11 @@
 a=int(input())
-if 90<=a<=100:
-    print("A")
-elif 80<=a<=89:
-    print("B")
-elif 70<=a<=79:
-    print("C")
-elif 60<=a<=69:
-    print("D")
+if a>=90:
+    print('A')
+elif a>=80:
+    print('B')
+elif a>=70:
+    print('C')
+elif a>=60:
+    print('D')
 else:
-    print("F")
+    print('F')
