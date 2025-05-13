552b6dd4582c3a767aa3814c9a82d85c07820c1c
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-05-14 00:29:21 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,10 +1,6 @@
 x=int(input())
 y=int(input())
-if x>0 and y>0:
-    print("1")
-elif x<0 and y>0:
-    print("2")
-elif x<0 and y<0:
-    print("3")
+if x>0:
+    print(1 if y>0 else 4)
 else:
-    print("4")
+    print(2 if y>0 else 3)  
