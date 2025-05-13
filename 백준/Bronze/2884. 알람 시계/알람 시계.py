45fc9db0ec8be5e318306063d7d18e3055df2d3f
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-05-14 00:40:25 +0900

    [Bronze III] Title: 알람 시계, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,9 +1,7 @@
 h,m=map(int,input().split())
-mm=(h*60+m)-45
-H=mm//60
-M=mm%60
-
-if mm<0:
-    print(23,60+mm)
-else:
-    print(H,M)
+result=h*60+m-45
+if result<0:
+    result+=24*60
+rh=result//60
+rm=result%60
+print(rh,rm)
