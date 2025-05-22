be8cf17659661d94b4c033457ec2c999d95c2641
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-05-22 21:30:17 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 91 ms, Memory: 60,544 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,9 +1,9 @@
 for k in range(10):
-    T=int(input())
+    a=int(input())
     arr=list(map(int,input().split()))
-    total=0
-    for i in range(2,T-2):
-        M=max([arr[i-2],arr[i-1],arr[i+1],arr[i+2]])
+    count=0
+    for i in range(2, a-2):
+        M=max(arr[i-2], arr[i-1], arr[i+1],arr[i+2])
         if arr[i]>M:
-            total+=arr[i]-M
-    print(f'#{k+1} {total}')
+            count+=arr[i]-M
+    print(f'#{k+1} {count}')
