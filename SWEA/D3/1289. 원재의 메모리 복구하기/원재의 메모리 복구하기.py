eb1fcaa0feac4663143bd7b84e686de81751cc3e
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-05-23 21:24:35 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 61 ms, Memory: 53,376 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -1,10 +1,10 @@
 T=int(input())
 for k in range(T):
-    a=input().strip()
-    prev='0'
+    a=input()
     count=0
+    start='0'
     for i in a:
-        if i!=prev:
+        if i!=start:
             count+=1
-            prev=i
+            start=i
     print(f'#{k+1} {count}')
