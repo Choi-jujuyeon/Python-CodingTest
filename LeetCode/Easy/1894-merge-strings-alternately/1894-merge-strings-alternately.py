89ae8dce782480e17078a3f9c5cc6586808e1d7d
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-06-17 01:01:21 +0900

    Delete LeetCode/Easy/1894-merge-strings-alternately directory

--- LeetCode/Easy/1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def mergeAlternately(self, word1, word2):
-        arr=[]
-        for i in range(max(len(word1),len(word2))): #길이최대
-
-            arr.append("" if i+1>len(word1) else word1[i] )
-            arr.append("" if i+1>len(word2) else word2[i] )
-        return "".join(arr)
