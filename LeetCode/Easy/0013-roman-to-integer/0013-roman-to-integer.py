b6bbbac2e3cd049d63dc8f9aff1cc0c9688c6840
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-06-17 01:50:29 +0900

    Upload Code - LeetHub

--- LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
+++ LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,5 +1,5 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
+class Solution(object):
+    def romanToInt(self, s):
         dic = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         result = 0
         for i in range(len(s) - 1):
