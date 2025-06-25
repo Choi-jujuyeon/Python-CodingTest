e577c426821e27b0e776aacbf5a9d65e438a606f
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-06-25 21:17:45 +0900

    Upload Code - LeetHub

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,7 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
-            return -1
+        l,r=0,len(nums)-1
+
+        while l<=r:
+            m=(l+r)//2
+            if target==nums[m]:
+                return m
+            
+            #왼쪽일 경우
+            if nums[l]<= nums[m]:
+                if target>nums[m] or target<nums[l]:
+                    l=m+1
+                else:
+                    r=m-1
+
+            #오른쪽일 경우
+            else:
+                if target<nums[m] or target > nums[r]:
+                    r=m-1
+                else:
+                    l=m+1
+        return -1
+
         
