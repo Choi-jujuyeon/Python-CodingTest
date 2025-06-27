6d757b0c849c6561d572c53eafc022c63ce9a1f3
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-06-27 16:08:17 +0900

    Upload Code - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.py
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.py
@@ -8,16 +8,3 @@ class Solution:
             return max(nums) +1
         else:
             return 0
-
-
-
-        if len(nums) == 1:
-            if min(nums) ==0:
-                return 1
-            else:
-                return 0
-        else:
-            for i in range(min(nums), max(nums)+1):
-                if i not in nums:
-                    return i
-            return max(nums) +1
