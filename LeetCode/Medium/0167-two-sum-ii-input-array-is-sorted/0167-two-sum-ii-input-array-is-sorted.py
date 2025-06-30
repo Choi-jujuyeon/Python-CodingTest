c23f6039be4182f2b0c668d30ebf09cc2f550191
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-01 02:13:01 +0900

    Upload Code - LeetHub

--- LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ LeetCode/Medium/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,23 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         #sol_1. hashMap
-        hashMap={}
-        for idx,num in enumerate(numbers):
-            t=target-num
-            if t in hashMap:
-                return [hashMap[t]+1, idx+1]
-            hashMap[num]=idx
-        
+        # hashMap={}
+        # for idx,num in enumerate(numbers):
+        #     t=target-num
+        #     if t in hashMap:
+        #         return [hashMap[t]+1, idx+1]
+        #     hashMap[num]=idx
+        #sol_2. pointer        
+        l,r=0,len(nums)-1
+        while l<r:
+            curSum= nums[l] + nums[r]
+            
+            # 값을 줄일경우: 오른쪽 포인터 (-)이동
+            if curSum>target:
+                r-=1
+            # 값을 늘릴 경우: 왼쪽 포인터 (+)이동
+            elif curSum < target:
+                l+=1
+            else:
+                return [l+1, r+1]
+            
