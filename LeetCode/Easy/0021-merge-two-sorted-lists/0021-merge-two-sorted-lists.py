57d0da963ab71e4d0666458429c9fd1b92b6be88
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-01 21:57:35 +0900

    Upload Code - LeetHub

--- LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,19 +5,21 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        tail = dummy 
-        while l1 and l2 : # not none
+        dummy = ListNode()     
+        tail = dummy        
+        while l1 and l2 : # not none => 그래야 비교할 수 있음
             if l1.val < l2.val:
-                tail.next=l1
-                l1= l1.next
+                tail.next=l1        # 작은거부터 tail에 연결
+                l1= l1.next         # l1 연결 후 업데이트
             else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
-        if l1:
+                tail.next = l2      
+                l2 = l2.next        # l2 연결 후 업데이트 
+            tail = tail.next        # tail 포인터는 상관없이 항상 업데이
+        # 둘 중 하나의 길이가 남이 있다면?
+        if l1:                      
             tail.next=l1
         elif l2:
+
             tail.next = l2
         return dummy.next
 
