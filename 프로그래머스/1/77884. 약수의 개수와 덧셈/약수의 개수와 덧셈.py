91fb01bfb141ed5f4b47cfd811272ca136ca1c50
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-14 22:05:33 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.63 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,17 +1,10 @@
-import math
 def solution(left, right):
     result=0
-    #약수의 개수를 알아야 한다. == 제곱근 활용
     for i in range(left,right+1):
-        count=0
-        for j in range(1,int(math.sqrt(i))+1):
-            if i%j==0:
-                if j==i//j:
-                    count+=1
-                else:
-                    count+=2
-        if count%2==0:
-            result+=i
-        else:
+        #제곱인지 확인
+        if int(i**0.5) == i**0.5:
             result-=i
+        else:
+            result+=i
     return result
+    
