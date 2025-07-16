4c69558d389ea969a0a21f28b9f3b558f43bfb03
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-16 23:14:05 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.77 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,16 +1,9 @@
 def solution(k, score):
-    stack,res=[],[]
+    q=[]
+    res=[]
     for i in score:
-        if stack==[] or len(stack)<k:
-            stack.append(i)         
-        else:
-            if stack[-1]<i:
-                stack.pop()
-                stack.append(i)
-        stack.sort(reverse=True)
-        res.append(stack[-1])
+        q.append(i)
+        if len(q)>k:
+            q.remove(min(q))
+        res.append(min(q))
     return res
-            
-                
-                
-    
