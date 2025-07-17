ad19167efca406b1a0a8eae329eef69a4bec94fb
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-17 23:00:48 +0900

    [level 1] Title: 모의고사, Time: 3.63 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,23 +1,18 @@
 def solution(answers):
-    arr = [
-        [1, 2, 3, 4, 5],                
-        [2, 1, 2, 3, 2, 4, 2, 5],       
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]  
-    ]
-    
-    res = []
+    arr=[[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5],[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    res=[]
     for num in arr:
-        count = 0
-        for i in range(len(answers)):
-            if answers[i] == num[i % len(num)]:
-                count += 1
+        count=0
+        for i in range(0,len(answers)):
+            if num[i%len(num)] == answers[i]:       #길이 무한히 체크
+                count+=1
         res.append(count)
-
-    max_score = max(res)
-    answer = []
-
+    m=max(res)
+    rr=[]
     for i in range(len(res)):
-        if res[i] == max_score:
-            answer.append(i + 1)
-
-    return answer
+        if res[i]== m:
+            rr.append(i+1)
+    return rr
+        
+    
+            
