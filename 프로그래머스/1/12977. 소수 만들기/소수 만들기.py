4ad70f9c7b061df7ea373686ed336570b37d65c5
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-18 02:53:22 +0900

    [level 1] Title: 소수 만들기, Time: 30.29 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -2,14 +2,10 @@ from itertools import combinations
 def solution(nums):
     count=0
     for i in combinations(nums,3):
-        pp=True
         for j in range(2, int(sum(i)**0.5)+1):
             if sum(i)%j==0:
-                pp=False
                 break
-        if pp:
-            count+=1
-        
-            
+        else:
+            count+=1          
     return count
     
