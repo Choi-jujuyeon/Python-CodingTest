5a12291771ff0d9614ca5d9fd1328c8c45b75da1
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-24 20:41:45 +0900

    Upload Code - LeetHub

--- LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,18 +1,16 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        maxArea = 0
-        stack=[]    #pair: (index,height)
+        maxArea =0
+        stack=[] #pair(index,height)
 
-        for i,h in enumerate(heights):          #i:막대의 위치, h:막대의 인덱스
-            start = i                           #일단 지금 막대는 자기 위치부터 시작한다고 생각
-            
-            while stack and stack[-1][1] >h:    #전에 본 막대가 지금보다 더 높으면?
-                                                #예전의 높은 막대들은 이제 더 못 커!그러니까 넓이를 계산
-                index,height = stack.pop()      # 전에 쌓았던 높은 막대 꺼내기
-                maxArea = max(maxArea, height * (i - index))  # 넓이 계산!
-                start= index                
-            stack.append((start, h))
-        for i, h in stack:
-            maxArea = max(maxArea, h*(len(heights)-i))
+        for i, h in enumerate(heights):
+            start = i
+            while stack and stack[-1][1]>h: #이전 높이가 현재 높이보다 클동안
+                index,height = stack.pop()  #계속pop
+                maxArea = max(maxArea, height*(i-index))
+                start = index
+            stack.append((start,h)) #(시작인덱스,높이) pair쌍으로 넣어줌
+        for i,h in stack:
+            maxArea = max(maxArea, h*(len(heights)-i)) #끝까지 높이가 작았음
         return maxArea
 
