476376777fc9d934c48108dce715c0faec7b4a94
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-25 16:56:40 +0900

    Upload Code - LeetHub

--- LeetCode/Medium/0883-car-fleet/0883-car-fleet.py
+++ LeetCode/Medium/0883-car-fleet/0883-car-fleet.py
@@ -1,11 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair =[[p,s] for p,s in zip(position,speed)]
+        pair = [[p,s] for p,s in zip(position,speed)]
 
-        stack=[]
-        for p, s in sorted(pair)[::-1]:
-            stack.append((target - p)/s)
-            if len(stack)>=2 and stack[-1] <=stack[-2]:
+        stack=[]    #target지점까지의 도착하는 시간을 저장
+
+        for p,s in sorted(pair)[::-1]:  #출발지점 내림차순 == 먼거부터 비교하자!
+            stack.append((target-p)/s)
+            if len(stack)>=2 and stack[-1]<=stack[-2]:
                 stack.pop()
-            
         return len(stack)
