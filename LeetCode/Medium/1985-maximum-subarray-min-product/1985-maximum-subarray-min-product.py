da7bfae107546bb181b1bcf8dc154c8a83b2262b
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-07-25 15:19:18 +0900

    Upload Code - LeetHub

--- LeetCode/Medium/1985-maximum-subarray-min-product/1985-maximum-subarray-min-product.py
+++ LeetCode/Medium/1985-maximum-subarray-min-product/1985-maximum-subarray-min-product.py
@@ -1,22 +1,27 @@
 class Solution:
     def maxSumMinProduct(self, nums: List[int]) -> int:
-        res =0
         stack=[]
         prefix=[0]
+        res=0
 
-        for n in nums:
-            prefix.append(prefix[-1] + n)
+        #누적합 체크
+        for i in nums:
+            prefix.append(prefix[-1]+i)
         
-        for i, n in enumerate(nums):
-            newStart= i
-            while stack and stack[-1][1] > n:   #내림차순일경우?
-                start, val = stack.pop()
-                total = prefix[i] - prefix[start]
-                res = max(res, val* total)
-                newStart = start 
-
+        #내림차순 발생 체크
+        for i,n in enumerate(nums):
+            newStart=i
+            while stack and stack[-1][1]>n:
+                idx,num = stack.pop()
+                total = prefix[i] - prefix[idx]
+                res = max(res,(total * num))
+                newStart = idx
             stack.append((newStart,n))
-        for start, val in stack:
-            total = prefix[len(nums)] - prefix[start]
-            res = max(res,val * total)
+        
+        #스택에 남아 있을 경우
+        for i,n in stack:
+            total = prefix[len(nums)] - prefix[i]
+            res = max(res,(total * n))
+
         return res % (10**9+7)
+
