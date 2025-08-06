d29af0f76d5df73e530dbb46392a120d7207f769
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-08-07 01:58:24 +0900

    Upload Code - LeetHub

--- LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,18 +1,4 @@
+from collections import deque
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        res=nums[0]
-        l,r=0,len(nums)-1     #인덱스 값저장
-
-        while l<=r: #r이 계속 클동안
-            if nums[l] < nums[r]:
-                res=min(res,nums[l])
-                break
-            m=(l+r)//2 #피봇 값 인덱스 저장
-            res=min(res,nums[m])
-            
-            if nums[m]>= nums[l]:
-                l=m+1
-            else:
-                r=m-1
-        return res
-
+        return min(nums)
