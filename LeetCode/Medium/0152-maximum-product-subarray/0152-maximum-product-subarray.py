c7237acb00c86fe158e58f4005c897e6607fae87
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-08-07 01:13:12 +0900

    Upload Code - LeetHub

--- LeetCode/Medium/0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ LeetCode/Medium/0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res = max(nums)
-        curMin, curMax = 1,1
-
+        curMin,curMax=1,1
+        res=max(nums) #원소 수 1개인게 -1 이런거일 수 있음
+        
         for n in nums:
             if n==0:
                 curMin,curMax = 1,1
                 continue
-            temp = curMax
-            curMax = max(n*curMax,n,n*curMin)
+            temp=curMax
+            curMax = max(n*curMin, n, n*curMax)
             curMin = min(n*temp,n,n*curMin)
             res=max(curMax,res)
         return res
