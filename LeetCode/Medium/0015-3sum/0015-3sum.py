acdbf8f6c1cfa476b56cb2d5083f1b1dddcce7dc
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-08-07 18:11:13 +0900

    Upload Code - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.py
+++ LeetCode/Medium/0015-3sum/0015-3sum.py
@@ -3,23 +3,19 @@ class Solution:
         res=[]
         nums.sort()
 
-        for idx,n in enumerate(nums):
-            #같은 값을 2번 사용할 생각 없음
-            if idx>0 and n==nums[idx-1]:
+        for i,a in enumerate(nums):
+            if i>0 and a == nums[i-1]:
                 continue
-            
-            l,r=idx+1, len(nums)-1
+            l,r = i+1, len(nums)-1
             while l<r:
-                threeSum = n+ nums[l]+nums[r]
-                if threeSum >0:
+                threeSum = a+nums[l]+nums[r]
+                if threeSum>0:
                     r-=1
                 elif threeSum<0:
                     l+=1
                 else:
-                    res.append([n,nums[l],nums[r]])
+                    res.append([a,nums[l],nums[r]])
                     l+=1
-                    while nums[l] == nums[l-1] and l<r:
+                    while nums[l]== nums[l-1] and l<r:
                         l+=1
         return res
-
-        return res  
