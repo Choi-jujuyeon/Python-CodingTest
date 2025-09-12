dda6881d34d56cb9467eb72ee45af80c9eb26319
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-09-12 16:19:46 +0900

    [level 2] Title: 올바른 괄호, Time: 8.87 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -8,8 +8,6 @@ def solution(s):
         if stack and i in dic:
             if stack[-1] == dic[i]:
                 stack.pop()
-            else:
-                stack.append(i)
         else:
             stack.append(i)
     return False if stack else True
