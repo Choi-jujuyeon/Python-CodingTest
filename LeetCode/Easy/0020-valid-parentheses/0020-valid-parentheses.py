204beb2de83d0a143a65ee76e3fd5240a7de21c3
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-09-25 17:04:01 +0900

    Upload Code - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,17 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         dic={
+            "]":"[",
             ")":"(",
-            "}":"{",
-            "]":"["
+            "}":"{"
         }
         stack=[]
         for i in s:
             if i in dic:
-                if stack and dic[i] == stack[-1]:
-                    stack.pop()
+                if stack:
+                    if dic[i] == stack[-1]:
+                        stack.pop()
+                    else: return False
                 else:
                     return False
             else:
                 stack.append(i)
         return False if stack else True
+
