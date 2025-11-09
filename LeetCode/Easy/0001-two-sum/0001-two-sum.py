920e8b32a2bc844fd2677e1c08584d08ac5a3444
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-11-10 02:09:13 +0900

    Upload Code - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,17 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        arr=[]
-        def backtracking(start):
-            #base_case
-            if len(arr) ==2:
-                if nums[arr[0]] + nums[arr[1]] == target:
-                    return arr[:]
-                return False
+        preMap={}
 
-            for i in range(start,len(nums)):                      
-                arr.append(i)
-                if backtracking(i+1):
-                    return arr
-                arr.pop()
-        return backtracking(0)
-        
+        for i,v in enumerate(nums):
+            if target-v in preMap:
+                return [i, preMap[target-v]]
+
+            preMap[v]=i
+            
