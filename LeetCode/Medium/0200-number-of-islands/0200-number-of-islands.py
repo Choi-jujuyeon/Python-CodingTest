6056dae199fccb62ccce27e2e2dfc0ba503217e8
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2025-11-21 01:05:34 +0900

    Upload Code - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
@@ -1,31 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+    
+    #### 초기값 설정
+        count=0 #섬이 몇개 있는지 체크
+        row_len = len(grid)
+        col_len = len(grid[0])
+        visited=[[False]*col_len for _ in range(row_len)]
+        dr=[0,1,0,-1]
+        dc=[1,0,-1,0]
         
-        rows,cols = len(grid), len(grid[0])
-        visit=set()
-        count=0
+        def dfs(r,c):
+            visited[r][c] =True
+            for i in range(4):
+                next_r = r + dr[i] # 4방향의 다음 좌표
+                next_c = c + dc[i]
 
-        def bfs(r,c):
-            q=deque()
-
-            visit.add((r,c))
-            q.append((r,c))
-
-            while q:
-                row, col = q.popleft()
-                directions=[[0,1],[1,0],[0,-1],[-1,0]]    
-                
-                for dr, dc in directions:
-                    r,c= row+dr, col+dc
-                    if r in range(rows) and c in range(cols) and grid[r][c]=="1" and (r,c) not in visit:
-                        q.append((r,c))
-                        visit.add((r,c))
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] =="1" and (r,c) not in visit:
-                    bfs(r,c)
+                if 0<= next_r<row_len and 0<= next_c < col_len:
+                    if grid[next_r][next_c] =="1":
+                        if not visited[next_r][next_c]:
+                            dfs(next_r,next_c)
+    #### 그리드를 순회
+        for i in range(row_len):
+            for j in range(col_len):
+            
+                #### "1"이고 아직 방문전이라면, dfs()처리 + count+=1
+                if grid[i][j] =="1" and not visited[i][j]:
+                    dfs(i,j)
                     count+=1
         return count
+        
