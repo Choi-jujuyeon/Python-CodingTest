e52fbb39146abfeffda23cee6ad6dff522245344
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-01-18 16:38:24 +0900

    [D1] Title: 중간값 찾기, Time: 58 ms, Memory: 52,736 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
@@ -1,3 +1,19 @@
-N=int(input())
-arr=sorted(list(map(int,input().split())))
-print(arr[N//2])
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] scores = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            scores[i] = sc.nextInt();
+        }
+       
+        Arrays.sort(scores);
+   
+        System.out.println(scores[N / 2]);
+    }
+}
