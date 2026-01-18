180193065e2732080f5c97ed011cc27273728e38
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-01-18 16:36:58 +0900

    [D1] Title: 자릿수 더하기, Time: 58 ms, Memory: 51,712 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -1,2 +1,17 @@
-a=input()
-print(sum([int(i) for i in a]))
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int sum = 0;
+
+        while (N > 0) {
+            sum += N % 10;  
+            N /= 10;  
+        }
+
+        System.out.println(sum);
+    }
+}
