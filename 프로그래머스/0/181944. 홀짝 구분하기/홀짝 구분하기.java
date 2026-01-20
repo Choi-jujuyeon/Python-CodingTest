cabe19590900a7008a52c785b3be299b4e4b87be
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-01-20 16:58:30 +0900

    [level 0] Title: 홀짝 구분하기, Time: 200.36 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -4,7 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String result = (n%2==0)?"is even": "is odd" ;
-        System.out.printf(n+" "+result);
+        // String result = (n%2==0)?"is even": "is odd" ;
+        // System.out.printf(n+" "+result);
+        System.out.printf(n + " is "+ ((n%2==0)? "even":"odd"));
     }
 }
