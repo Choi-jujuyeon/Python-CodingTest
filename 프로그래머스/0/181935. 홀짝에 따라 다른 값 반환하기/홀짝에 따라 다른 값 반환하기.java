ead6e1b10471cb9645834011befac003860a5d7a
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-01-20 16:50:58 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -1,18 +1,9 @@
 class Solution {
     public int solution(int n) {
-        int sum=0;
-        //홀수라면 n이하의 홀수인 모든 양의 정수 합
-        if(n%2==0){
-            for(int i=2; i<=n; i+=2){
-                sum+=i*i;
-            }
-        }else{
-            for(int i=1; i<=n; i+=2){
-                sum+=i;
-            }
-        }
-        return sum;
-        
-        //짝수라면 n이하의 짝수 중 정수의 제곱 합
+        int result =0;
+        for(int i=n; i>=0; i-=2)
+            result += (n%2==0)? i*i:i;
+        return result;
     }
+    
 }
