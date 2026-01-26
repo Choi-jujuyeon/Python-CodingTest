f29cf32da06bcfe8ea96367c87d5c51844529d11
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-01-26 23:41:02 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 268 ms, Memory: 19228 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n; i++) {
+        	
+        	//공백 출력
+        	for(int j=0; j<i; j++) {
+        		System.out.print(" ");
+        	}
+        	
+        	//별찍기
+        	for(int j=n; j>i; j--) {
+        		System.out.print("*");
+        	}
+        	System.out.println();
+        	
+        }
+    }
+}
