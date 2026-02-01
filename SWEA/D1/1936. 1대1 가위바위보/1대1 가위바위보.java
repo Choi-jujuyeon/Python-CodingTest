18a4b717dd6351b25ed70afa486ea774e5de9f6b
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-01 23:57:28 +0900

    [D1] Title: 1대1 가위바위보, Time: 94 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        
-        if ((A==1 && B==3) || (A==2 && B==1) || (A==3 && B==2)){
-            System.out.println("A");
-        }else{
-            System.out.println("B");
-        }
-    }    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		
+		//A가 이기는 조건만
+		if((a==1 && b==3) ||(a==2&&b==3) || (a==3 &&b==2))
+			System.out.println("A");
+		else
+			System.out.println("B");		
+	}
 }
