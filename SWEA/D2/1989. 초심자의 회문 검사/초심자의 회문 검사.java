5beff26f6f3d471eb290890fef40773363fa404a
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-04 13:13:33 +0900

    [D2] Title: 초심자의 회문 검사, Time: 95 ms, Memory: 26,880 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -3,30 +3,28 @@ import java.util.Deque;
 import java.util.Scanner;
 
 public class Solution {
-	
-	
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	int T = sc.nextInt();
-    	
-    	for(int i=1; i<=T; i++) {
-    		String s = sc.next();
-    		System.out.println("#"+i+" "+check(s));
-    	}
-    }
-    
-    public static int check(String s) {
-    	Deque<Character> dq = new ArrayDeque<>();
-    	
-    	for(int i=0; i<s.length(); i++) 
-    		dq.addLast(s.charAt(i));
-    	
-    	while(dq.size()>1) {
-    		char lp = dq.pollFirst();
-    		char rp = dq.pollLast();
-    		if(lp != rp ) return 0;
-    	}
-    	
-		return 1;
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for(int tc=1; tc<=T; tc++) {
+			boolean result = true;
+			String[] s = sc.next().split("");
+			
+			for(int i=0; i<s.length/2; i++) {
+				if(s[i].equals(s[s.length-1-i])) {
+					continue;
+				}else {
+					result=false;
+					break;
+				}
+			}
+			if(result)
+				System.out.println("#"+ tc+" "+1);
+			else
+				System.out.println("#"+tc+" "+0);
+			
+		}
+		
+	}
 }
