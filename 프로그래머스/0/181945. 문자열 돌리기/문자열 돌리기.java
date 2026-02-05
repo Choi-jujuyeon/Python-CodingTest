cf85f4495ce87da5c3505bf7854e8806aaa2f774
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-05 16:59:09 +0900

    [level 0] Title: 문자열 돌리기, Time: 182.22 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -4,7 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        for(int i=0; i<a.length(); i++)
-            System.out.println(a.charAt(i));
+        for(char ch: a.toCharArray())
+            System.out.println(ch);
     }
 }
