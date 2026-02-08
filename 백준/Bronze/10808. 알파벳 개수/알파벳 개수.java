f790934487088e25c53da5b7952e3175ac623cdb
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-08 21:34:38 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 180 ms, Memory: 18300 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.next();
+		int[] arr = new int[26];
+		for(int i=0; i<s.length(); i++) {
+			arr[s.charAt(i)-'a'] +=1;
+		}
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
