0377dd91f850d3b95a22b46d9366aea98af4fddf
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-12 14:45:05 +0900

    [D2] Title: 두 개의 숫자열, Time: 96 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -3,35 +3,33 @@ import java.util.Scanner;
 class Solution{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		 int T = sc.nextInt();
-		 
-		 for(int tc=1; tc<T+1; tc++) {
-			 int N = sc.nextInt();
-			 int M = sc.nextInt();
-			 
-			 int[] a = new int[N];
-			 int[] b = new int[M];
-			 
-			 for(int i=0; i<N; i++) a[i]= sc.nextInt();
-			 for(int i=0; i<M; i++) b[i] =sc.nextInt();
-			 
-			 int max = Integer.MIN_VALUE;
-			 
-			 //M길이가 항상 큰 길이로 가정하기!
-			 if(N>M) {
-				 int[] tmp = a; a=b; b=tmp;
-				 int t=N; N=M; M=t;
-			 }
+		int T = sc.nextInt();
+		
+		for(int tc=1; tc<=T;tc++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			int[] a = new int[N];
+			int[] b = new int[M];
+			
+			for(int i=0; i<N; i++) a[i]=sc.nextInt();
+			for(int i=0; i<M; i++) b[i]=sc.nextInt();
+			
+			//n이 항상 작게 설정
+			if(N>M) {
+				int[] tmp=a; a=b; b=tmp;
+				int t=N; N=M; M=t;
+			}
+			int max=0;
+			//이동
 			 for(int i=0; i<=M-N; i++) {
 				 int sum=0;
 				 for(int j=0; j<N; j++) {
-					 sum+=a[j] * b[i+j];
+					 sum+=a[j]*b[j+i];
 				 }
-				 if(sum>max) max=sum;
+				 if(max<sum) max=sum;
 			 }
-			 System.out.println("#"+tc+" "+max);
-				
-		 }
-		
+			 System.out.println("#"+tc+ " "+max);
+		}
 	}
 }
