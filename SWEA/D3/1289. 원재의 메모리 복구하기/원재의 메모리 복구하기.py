4300e8a77c1b27063cf2bd2807c070488a805962
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-18 11:56:21 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 61 ms, Memory: 53,376 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -1,10 +1,26 @@
-T=int(input())
-for k in range(T):
-    a=input()
-    count=0
-    start='0'
-    for i in a:
-        if i!=start:
-            count+=1
-            start=i
-    print(f'#{k+1} {count}')
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		sc.nextLine();
+		for(int tc=1; tc<=T; tc++) {
+			
+			String[] s= sc.nextLine().split("");
+			
+			String check="0";
+			int cnt=0;
+			for(int i=0; i<s.length; i++) {
+				if(s[i].equals(check)) continue;
+				else {
+					check=s[i];
+					cnt++;
+				}
+			}
+			System.out.println("#"+tc+" "+cnt);
+		}
+	}
+}
