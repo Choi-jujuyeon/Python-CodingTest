629d8ed08d01a8612bde8de2ce9fcc66b3e67e9e
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-18 21:33:08 +0900

    [D3] Title: 퍼펙트 셔플, Time: 103 ms, Memory: 29,952 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -1,37 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
+import java.util.Scanner;
 
-public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-
-		for(int i=1; i<T+1; i++) {
-			
-			br.readLine();
-			String[] arr = br.readLine().split(" ");
-
-			int n = arr.length;
-			int mid = (n + 1) / 2;
-
-			int left= 0;
-			int right= mid;
-
-			StringBuilder sb = new StringBuilder();
-
-			while (left<mid || right < n) {
-			    if (left<mid) sb.append(arr[left++]).append(' ');
-			    if (right<n) sb.append(arr[right++]).append(' ');
-			}
-
-			sb.setLength(sb.length() - 1);
-			System.out.println("#"+i+" "+sb);
-
-		}
+class Solution{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int T = sc.nextInt();
+		
+		for(int tc=1; tc<=T; tc++) {
+			int N = sc.nextInt();
+			sc.nextLine();
+			String[] arr = sc.nextLine().split(" ");
+			
+			int start = (N + 1) / 2;
+            System.out.print("#" + tc + " ");
+            
+            for(int i=0; i<start; i++) {
+            	System.out.print(arr[i] +" ");
+            	
+            	if(start+i<N)
+            		System.out.print(arr[start+i]+" ");
+            }
+            System.out.println();
+		}
 	}
 }
