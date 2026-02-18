e3e45a0cf02faf4d290c9e9755701abc1af7ee2b
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-18 23:39:45 +0900

    [D2] Title: 과일 등급 분류, Time: 100 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D2/26059. 과일 등급 분류/과일 등급 분류.java
+++ SWEA/D2/26059. 과일 등급 분류/과일 등급 분류.java
@@ -1,45 +1,54 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+class Solution{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for(int tc=1; tc<=T; tc++) {
+			int N =sc.nextInt();
+			int lo = sc.nextInt();
+			int hi = sc.nextInt();
+			
+			int[] arr = new int[N];
+			for(int i=0; i<N; i++) arr[i] = sc.nextInt();
+			
+			Arrays.sort(arr);
+			
+			//k1 k2 어디에 둘지 + 같은 무게 쪼개면 안돼
+			//몇 개로 나눠야 할지 모를때는 ArrayList를 사용해라
+			ArrayList<Integer> arr2 = new ArrayList<>();
+			for(int i=0; i<N-1; i++) {
+				if(arr[i]!=arr[i+1])
+					arr2.add(i+1); 
+			}
+//			System.out.println(arr2); //여기서 2개만 택하면 됌
+			
+			int min = Integer.MAX_VALUE;
+			for(int i=0; i<arr2.size(); i++) {
+				int cut1 = arr2.get(i);
+				
+				for(int j=i+1; j<arr2.size(); j++) {
+					int cut2 = arr2.get(j);
+					
+					//객수만 체크
+					int low = cut1;
+					int mid = cut2-cut1;
+					int high = N-cut2;
+							
+					if(low>=lo && mid>=lo && high>=lo &&low<=hi && mid<=hi && high<=hi) {
+						int maxG = Math.max(low, Math.max(mid, high));
+						int minG = Math.min(low, Math.min(mid, high));
+						
+						if(min> maxG-minG) min = maxG-minG;
+					}
+						
+				}
+			}
+			System.out.println("#"+tc+" "+(min==Integer.MAX_VALUE? -1: min));
 
-        for (int tc = 1; tc <= T; tc++) {
-            int N = sc.nextInt();
-            int lo = sc.nextInt();
-            int hi = sc.nextInt();
-            int[] ws = new int[N];
-            
-            for (int i = 0; i < N; i++) ws[i] = sc.nextInt();
-            Arrays.sort(ws);
-            
-            int result = Integer.MAX_VALUE;
-            
-            for (int i = lo; i <= N - 2 * lo; i++) {
-                int lowCount = i;
-
-                if (lowCount > hi) continue;
-                if (ws[i - 1] == ws[i]) continue;
-
-                for (int j = i + lo; j <= N - lo; j++) {
-                    int midCount = j - i;
-                    int highCount = N - j;
-
-                    if (midCount > hi || highCount > hi) continue;
-                    if (ws[j - 1] == ws[j]) continue;
-
-                    int mx = Math.max(lowCount, Math.max(midCount, highCount));
-                    int mn = Math.min(lowCount, Math.min(midCount, highCount));
-                    result = Math.min(result, mx - mn);
-                }
-            }
-
-            if (result == Integer.MAX_VALUE) result = -1;
-            System.out.println("#" + tc + " " + result);
-        }
-
-        sc.close();
-    }
+		}
+	}
 }
