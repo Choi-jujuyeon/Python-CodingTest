1c57ce4ffafe222ba47d3dfc6ef3d4562dfcf52d
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-02-26 13:41:26 +0900

    [Bronze III] Title: ACM 호텔, Time: 224 ms, Memory: 18456 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for (int tc = 0; tc < T; tc++) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();  
+            int N = sc.nextInt();
+
+            int floor = (N - 1) % H + 1; 
+            int room  = (N - 1) / H + 1;  
+
+            System.out.printf("%d%02d\n", floor, room);
+        }
+
+    }
+}
