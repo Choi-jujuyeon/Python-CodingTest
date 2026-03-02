20e242951ebff92cb102d9b929ea096a2b962a9a
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-03-02 23:53:50 +0900

    [Silver V] Title: 다리 놓기, Time: 128 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine().trim());
+	
+		for(int tc=0; tc<T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());			
+			int N = Integer.parseInt(st.nextToken());
+    		int M = Integer.parseInt(st.nextToken());
+    		
+    		long ans=1;
+    		for(int i=1; i<=N; i++) {
+    			ans = ans* (M-i+1)/i;
+    		}
+    		System.out.println(ans);
+		}
+		
+	}
+}
