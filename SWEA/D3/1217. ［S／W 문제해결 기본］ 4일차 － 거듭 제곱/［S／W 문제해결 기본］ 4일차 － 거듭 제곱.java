17b38a6d41c6eac9ac97708e2ad97497cb4bcb9f
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-03-05 11:54:09 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 96 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
-public class Solution{
+class Solution{
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
-		for(int tc=1; tc<11; tc++) {
-			sc.nextInt();
-			 int a = sc.nextInt();
-			 int b = sc.nextInt();
-			System.out.println("#"+tc+" "+check(a,b));
+		
+		for(int tc=1; tc<=10; tc++) {
+			int T = sc.nextInt();
+			
+			int C = sc.nextInt();
+			int N = sc.nextInt();
+
+			System.out.println("#"+tc+" "+check(C,N));
 		}
 	}
-	
-	public static int check(int a, int b) {
-		//basecase*
-		if(b==0)
-			return 1;
+	static int check(int c, int n) {
+		if(n==0) return 1;
 		
-		return a*check(a,b-1);
+		int tmp = check(c, n/2);
+		if(n%2 ==1) return tmp * tmp*c;
+		
+		return tmp *tmp;
 	}
 }
