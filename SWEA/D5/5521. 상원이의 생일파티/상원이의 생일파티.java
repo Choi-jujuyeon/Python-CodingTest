f66288423f12c11a61771a301594afd28c9b3f4b
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-03-11 14:05:41 +0900

    [D5] Title: 상원이의 생일파티, Time: 155 ms, Memory: 42,240 KB -BaekjoonHub

--- SWEA/D5/5521. 상원이의 생일파티/상원이의 생일파티.java
+++ SWEA/D5/5521. 상원이의 생일파티/상원이의 생일파티.java
@@ -1,61 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-class Solution {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int T = Integer.parseInt(br.readLine().trim());
-
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine().trim());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-
-            // 친구 관계 저장할 그래프
-            ArrayList<Integer>[] graph = new ArrayList[N + 1];
-            for (int i = 1; i <= N; i++) {
-                graph[i] = new ArrayList<>();
-            }
-
-            // 친구 관계 입력
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine().trim());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-
-                graph[a].add(b);
-                graph[b].add(a);
-            }
-
-            // BFS 준비
-            boolean[] visited = new boolean[N + 1];
-            int[] depth = new int[N + 1];
-
-            Queue<Integer> q = new LinkedList<>();
-            q.offer(1);         // 상원이부터 시작
-            visited[1] = true;
-
-            int count = 0;
-
-            while (!q.isEmpty()) {
-                int cur = q.poll();
-
-                for (int next : graph[cur]) {
-                    if (!visited[next]) {
-                        visited[next] = true;
-                        depth[next] = depth[cur] + 1;
-
-                        // 친구 또는 친구의 친구만 초대
-                        if (depth[next] <= 2) {
-                            count++;
-                            q.offer(next);
-                        }
-                    }
-                }
-            }
-
-            System.out.println("#" + tc + " " + count);
-        }
-    }
+class Solution{
+	
+	static boolean[] visited;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		int T = sc.nextInt();
+		for(int tc=1; tc<=T; tc++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			visited = new boolean[N+1];
+			
+			ArrayList<Integer>[] arr = new ArrayList[N+1];
+			for(int i=0; i<=N; i++) {
+				arr[i] = new ArrayList<>();
+			}
+			
+			for(int i=0; i<M; i++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				
+				arr[a].add(b);
+				arr[b].add(a);
+			}
+			
+			for(int friend: arr[1]) {
+				visited[friend] = true;
+				
+				for(int friendF: arr[friend]){
+					if(friendF !=1)
+						visited[friendF] = true;
+				}
+			}
+			
+			int cnt =0;
+			for(int i=2; i<=N; i++) {
+				if(visited[i]) cnt++;
+			}
+			System.out.println("#"+tc+" "+cnt);
+		}
+	}
 }
