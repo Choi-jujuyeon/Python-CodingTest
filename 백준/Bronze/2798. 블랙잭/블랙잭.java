6259ffbc1fe76a365a14dae670124c760258d3cd
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-03-23 16:42:43 +0900

    [Bronze II] Title: 블랙잭, Time: 180 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,32 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 class Main{
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] s =br.readLine().split(" ");
-		
-		int N =Integer.parseInt(s[0]);
-		int sum = Integer.parseInt(s[1]);
-		
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int totalSum = sc.nextInt();
 		int[] arr = new int[N];
-		String[] arr2 = br.readLine().split(" ");
-		for(int i=0; i<N; i++) arr[i] = Integer.parseInt(arr2[i]);
+		for(int i=0; i<N; i++) 
+			arr[i] = sc.nextInt();
 		
-		
-		int max=0;
+		int max = Integer.MIN_VALUE;
+		// 이중에서 3개를 뽑아서 21을 넘지 않는 최대의 수를 구해라!
 		for(int i=0; i<N; i++) {
-			for(int j=i+1; j<N; j++) {
+			for(int j=i+1; j<N; j++){
 				for(int k=j+1; k<N; k++) {
-					int total = arr[i]+arr[j]+arr[k];
-					if(total<=sum && total>max) max=total;
-					
+					int a = arr[i]+arr[j]+arr[k];
+					if(max<a && totalSum>=a) max = a;
 				}
 			}
 		}
-		System.out.println(max);
 		
-	}
+		
+		System.out.println(max);
+	}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 }
