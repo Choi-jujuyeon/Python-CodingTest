8cf53ae23b8ecfbf9dac434f38c68d3a95594c7b
Choi-jujuyeon <108813331+Choi-jujuyeon@users.noreply.github.com>
2026-03-23 15:34:37 +0900

    [Bronze III] Title: 웰컴 키트, Time: 188 ms, Memory: 18364 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
 class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int[] arr = new int[6];
-
-        for(int i=0; i<6; i++) arr[i] = sc.nextInt();
-        int T = sc.nextInt();
-        int P = sc.nextInt();
-
-        long tCnt = 0;
-        for(int i=0; i<6; i++) {
-            tCnt += (arr[i] + T - 1) / T;  
-        }
-
-        System.out.println(tCnt);
-        System.out.println((N / P) + " " + (N % P));
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] tmp = new int[6];
+		for(int i=0; i<6; i++) 
+			tmp[i] = sc.nextInt();
+		int T = sc.nextInt();
+		int P = sc.nextInt();
+		
+		int shirt = 0;
+		int ans =0;
+		
+		for(int i=0; i<6; i++) {
+			shirt += (tmp[i] + T -1) /T;
+			ans +=tmp[i];
+		}
+		
+		int bundlePens = 0;
+		int Pen =0;
+		
+		bundlePens = ans/P;
+		Pen = ans%P;
+		
+		System.out.println(shirt);
+		System.out.println(bundlePens + " "+Pen);
+		
+		
+	}
 }
